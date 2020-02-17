46737c193d35174768415dc715d2a0747836c616
Alexey <rohtuasad@gmail.com>
2020-02-17 23:35:28 +0300

    Initialize Spring Application

---
+++ src/main/java/ru/rohtuasad/springjokesapp/SpringJokesAppApplication.java
@@ -0,0 +1,13 @@
+package ru.rohtuasad.springjokesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJokesAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJokesAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/rohtuasad/springjokesapp/SpringJokesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.rohtuasad.springjokesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJokesAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
